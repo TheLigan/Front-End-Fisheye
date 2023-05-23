729cfdc9803d92a1edd663d1eca243de507017d9
Verdon Xavier <126431314+TheLigan@users.noreply.github.com>
2023-05-23 06:27:48 +0200

    V0.1 lancement du projet
    mise en place de l'environnement de travail
    ajout de fletch + Mise à jour de index.js/constructeur photographer

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,33 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement("article");
+        const img = document.createElement("img");
+        const h2 = document.createElement("h2");
+        const h3 = document.createElement("h3");
+        const p = document.createElement("p");
+        const h4 = document.createElement("h4");
+        const a = document.createElement("a");
+
+        img.setAttribute("src", picture);
         h2.textContent = name;
+        h3.textContent = `${city} , ${country}`;
+        p.textContent = tagline;
+        h4.innerText = `${price} € / jour`;
+        a.setAttribute("href", `photographer.html?id=${id}`);
+        a.setAttribute("aria-label", `Lien vers le portfolio de ${name}`);
+
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(h3);
+        article.appendChild(p);
+        article.appendChild(h4);
+        a.appendChild(article);
+
+        return (a);
     }
-    return { name, picture, getUserCardDOM }
+    return { id, name, picture, city, price, country, medias, getUserCardDOM, }
 }
